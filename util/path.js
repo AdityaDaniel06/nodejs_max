43134b9440c5350ac1e9a5eb1a08719282a821a2
AdityaDaniel06 <danieladitya327@gmail.com>
2024-09-14 20:49:29 +0530

    new changes

--- util/path.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-
-//constructing a path for the root directory
-// module.exports = path.dirname(process.mainModule.filename);
-module.exports = path.dirname(require.main.filename);
